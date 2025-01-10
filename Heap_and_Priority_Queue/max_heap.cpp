811885b5d453737f7a064c7f0ff1947530a7b118
M TOQEER ZIA <muhammadtoqeerzia586694@gmail.com>
2025-01-10 23:26:10 +0500

    heap data setructure

--- Heap_and_Priority_Queue/max_heap.cpp
+++ Heap_and_Priority_Queue/max_heap.cpp
@@ -1,25 +1,83 @@
-#include<iostream>
-#include<queue>
+#include <iostream>
 using namespace std;
-int main(){
-    priority_queue<int>PQ;
-    PQ.push(10);
-    PQ.push(11);
-    PQ.push(56);
-    PQ.push(23);
-    PQ.push(5);
-    PQ.push(68);
-    PQ.push(55);
-    PQ.push(1);
-
-    cout<<"TOP: "<<PQ.top()<<" ";
-    PQ.pop();
-    cout<<"TOP: "<<PQ.top()<<" ";
-    PQ.pop();
-    while (!PQ.empty())
-    {
-        cout<<PQ.top()<<" ";
-        PQ.pop();
+class Heap
+{
+public:
+    int arr[100];
+    int size;
+    Heap(){
+        size=0;
+        arr[0]=-1;
     }
-    
+
+    void print()
+    {
+        for (int i = 0; i < size; i++)
+        {
+           cout<<arr[i]<<" ";
+        }
+        cout<<"\n";
+        return;
+    }
+    void insert(int val)
+    {
+        size++;
+        int index = size;
+        arr[index] = val;
+        // swaping the parent element to below if the parent value is lesser then the child node
+        while (index > 1)
+        {
+            int parent = index / 2;
+            if (arr[parent] < arr[index])
+            {
+                swap(arr[parent], arr[index]);
+                index = parent;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+    void Delete()
+    {
+        if (size == 0)
+        {
+            cout<<"there is nothing to delete!";
+            return;
+        }
+        int i = 1;
+        arr[i] = arr[size];
+        while (i < size)
+        {
+            int leftIndex = 2 * i;
+            int rightIndex = 2 * i + 1;
+            if (leftIndex < size && arr[i] < arr[leftIndex])
+            {
+                swap(arr[i], arr[leftIndex]);
+                i = leftIndex;
+            }
+            else if (rightIndex < size && arr[i] < arr[rightIndex])
+            {
+                swap(arr[i], arr[rightIndex]);
+                i = rightIndex;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+};
+int main()
+{
+    Heap h1;
+    h1.insert(34);
+    h1.insert(23);
+    h1.insert(56);
+    h1.insert(76);
+    h1.insert(21);
+    h1.insert(90);
+    h1.insert(34);
+    h1.print();
 }
