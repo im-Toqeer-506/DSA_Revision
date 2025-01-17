45cd9e27f8bac15ef701dedb5121f5abd0c8faed
M TOQEER ZIA <muhammadtoqeerzia586694@gmail.com>
2025-01-17 12:56:18 +0500

    bfs iterative approch

--- Graph/iter_bfs.cpp
+++ Graph/iter_bfs.cpp
@@ -24,6 +24,30 @@ public:
             cout<<endl;
         }
     }
+    void bfs(int startNode) {
+        // 1. Visited
+        unordered_map<int, bool> visited;
+        // 2. Queue
+        queue<int> q;
+        // Start BFS
+        q.push(startNode);
+        visited[startNode] = true;
+        cout << "BFS Traversal: ";
+        while (!q.empty()) {
+            // 3. Current Node
+            int currentNode = q.front();
+            q.pop();
+            cout << currentNode << " ";
+            // 4. Adjacency List
+            for (auto neighbor : adj[currentNode]) {
+                if (!visited[neighbor]) {
+                    q.push(neighbor);
+                    visited[neighbor] = true;
+                }
+            }
+        }
+        cout << endl;
+    }
 };
 int main()
 {
@@ -41,6 +65,6 @@ int main()
         g.addEdge(u,v,0);
     }
     g.PrintEdgeList();
-    
+    g.bfs(0);
     
 }
