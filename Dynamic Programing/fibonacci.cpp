3a16e6d3b0503124b497f8832b652368995f3d79
M TOQEER ZIA <muhammadtoqeerzia586694@gmail.com>
2025-03-12 11:28:25 +0500

    DP fibonacci using memeorization

--- Dynamic Programing/fibonacci.cpp
+++ Dynamic Programing/fibonacci.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
 #include <unordered_map>
+
 using namespace std;
+
 int fibo(int n, unordered_map<int, int>& memo) {
     if (n == 0 || n == 1)
         return n; 
-
     if (memo.find(n) != memo.end()) 
         return memo[n];
-
-   
     memo[n] = fibo(n - 1, memo) + fibo(n - 2, memo);
     return memo[n];
 }
-
 int fibo(int n) {
     unordered_map<int, int> memo; 
     return fibo(n, memo);
 }
-
 int main() {
-    cout << fibo(6); 
+    cout << fibo(6);
 }
