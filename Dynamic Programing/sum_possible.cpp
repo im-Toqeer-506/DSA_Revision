75a5ae3371fb11acc754ace5c3c5e617e280030c
M TOQEER ZIA <muhammadtoqeerzia586694@gmail.com>
2025-03-25 21:31:22 +0500

    min_change using DP

--- Dynamic Programing/sum_possible.cpp
+++ Dynamic Programing/sum_possible.cpp
@@ -2,13 +2,19 @@
 #include <vector>
 #include <unordered_map>
 using namespace std;
-bool sumPossibleHelper(int amount, vector<int> &numbers, unordered_map<int, bool> &memo) {
-    if (amount == 0) return true;  
-    if (amount < 0) return false;  
-    if (memo.find(amount) != memo.end()) return memo[amount];  
-    for (int num : numbers) {
+bool sumPossible(int amount, vector<int> &numbers, unordered_map<int, bool> &memo)
+{
+    if (amount == 0)
+        return true;
+    if (amount < 0)
+        return false;
+    if (memo.find(amount) != memo.end())
+        return memo[amount];
+    for (int num : numbers)
+    {
         int subAmount = amount - num;
-        if (sumPossibleHelper(subAmount, numbers, memo)) {
+        if (sumPossible(subAmount, numbers, memo))
+        {
             memo[amount] = true;
             return true;
         }
@@ -16,18 +22,22 @@ bool sumPossibleHelper(int amount, vector<int> &numbers, unordered_map<int, bool
     memo[amount] = false;
     return false;
 }
-bool sumPossible(int amount, vector<int> &numbers) {
+bool sumPossible(int amount, vector<int> &numbers)
+{
     unordered_map<int, bool> memo;
-    return sumPossibleHelper(amount, numbers, memo);
+    return sumPossible(amount, numbers, memo);
 }
-int main() {
-    vector<int> numbers = {5, 3, 4, 7};  
-    int amount = 7;                       
-    if (sumPossible(amount, numbers)) {
-        cout << "True" << endl;
-    } else {
-        cout << "False" << endl;
+int main()
+{
+    vector<int> numbers = {2, 3, 5};
+    int amount = 8;
+    if (sumPossible(amount, numbers))
+    {
+        cout << "Yes, sum is possible!" << endl;
     }
-
+    else
+    {
+        cout << "No, sum is not possible!" << endl;
+    }
     return 0;
 }
